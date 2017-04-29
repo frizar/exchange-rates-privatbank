7212430c654755beefa3c8f6a9b345de2a044c3d
frizar <vkfrizar@gmail.com>
2017-04-29 11:16:52 +0300

    add simple converter

--- popup.js
+++ popup.js
@@ -4,30 +4,38 @@
     // const extPage = chrome.extension.getBackgroundPage(); // debug page
     // extPage.console.log(extPage);
 
-    let container = document.querySelector('.container');
+    let table = document.querySelector('.table');
+    let currencies = [];
+    let currencyType = document.querySelector('select#currencyType');
+    let currencyValue = document.querySelector('input#currencyValue');
+    let currencyIndex = -1;
+    let uahResult = document.querySelector('span#uahResult');
 
     let xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
 
     xhr.addEventListener('load', function() {
-        let response = {};
-
         // check valid JSON
         try {
-            response = JSON.parse(this.responseText);
+            currencies = JSON.parse(this.responseText);
             // extPage.console.log(response);
         } catch(err) {
             return onError();
         }
 
-        let result = '<table><tr><th>Валюта</th><th>Покупка</th><th>Продажа</th></tr>';
-        response.forEach(currency => {
-            result += `<tr><td>${currency.ccy}</td><td>${currency.buy}</td><td>${currency.sale}</td></tr>`;
-        });
-        result += '</table>';
+        currencyIndex = getCurrencyIndex(currencyType.value);
 
-        container.innerHTML = result;
+        table.innerHTML = createTableHTML();
+
+        currencyType.addEventListener('change', function() {
+            currencyIndex = getCurrencyIndex(this.value);
+            showUAH(currencyValue.value);
+        });
+
+        currencyValue.addEventListener('change', function() {
+            showUAH(this.value);
+        });
     });
 
     xhr.addEventListener('error', onError);
@@ -35,6 +43,43 @@
     xhr.send();
 
     function onError() {
-        container.innerHTML = 'Не удалось получить данные :( Попробуйте еще раз.';
+        table.innerHTML = 'Не удалось получить данные :( Попробуйте еще раз.';
+    }
+
+    function createTableHTML() {
+        let result = '<table><tr><th>Валюта</th><th>Покупка</th><th>Продажа</th></tr>';
+
+        currencies.forEach(currency => {
+            result += `<tr><td>${currency.ccy}</td><td>${currency.buy}</td><td>${currency.sale}</td></tr>`;
+        });
+
+        result += '</table>';
+
+        return result;
+    }
+
+    function getCurrencyIndex(type) {
+        for (let i = 0; i < currencies.length; i++) {
+            if (currencies[i].ccy === type) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    function calcUAH(value) {
+        if (currencyIndex === -1) {
+            return {buy: 0, sale: 0};
+        }
+
+        let buy = (currencies[currencyIndex].buy * value).toFixed(5);
+        let sale = (currencies[currencyIndex].sale * value).toFixed(5);
+        return {buy, sale};
+    }
+
+    function showUAH(value) {
+        let UAH = calcUAH(value);
+        uahResult.innerText = `${UAH.buy} / ${UAH.sale}`;
     }
 }());
