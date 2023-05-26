281f5c20b736c39c4680bdd9e76dbfff1e392efc
frizar <vkfrizar@gmail.com>
2023-05-26 15:28:19 +0300

    update to manifest v3, style update, refactoring

--- popup.js
+++ popup.js
@@ -1,85 +1,69 @@
 'use strict';
 
-(function() {
-    // const extPage = chrome.extension.getBackgroundPage(); // debug page
-    // extPage.console.log(extPage);
-
-    let table = document.querySelector('.table');
+(async function () {
+    let table = document.querySelector('#table');
     let currencies = [];
-    let currencyType = document.querySelector('select#currencyType');
-    let currencyValue = document.querySelector('input#currencyValue');
+    let currencyType = document.querySelector('#currencyType');
+    let currencyValue = document.querySelector('#currencyValue');
     let currencyIndex = -1;
-    let uahResult = document.querySelector('span#uahResult');
+    let uahResult = document.querySelector('#uahResult');
 
-    let xhr = new XMLHttpRequest();
+    try {
+        let response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
 
-    xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11');
-
-    xhr.addEventListener('load', function() {
-        // check valid JSON
-        try {
-            currencies = JSON.parse(this.responseText);
-            // extPage.console.log(response);
-        } catch(err) {
-            return onError();
-        }
+        currencies = await response.json();
 
         currencyIndex = getCurrencyIndex(currencyType.value);
 
         table.innerHTML = createTableHTML();
 
-        currencyType.addEventListener('change', function() {
+        currencyType.addEventListener('change', function () {
             currencyIndex = getCurrencyIndex(this.value);
+
             showUAH(currencyValue.value);
         });
 
-        currencyValue.addEventListener('change', function() {
+        currencyValue.addEventListener('change', function () {
             showUAH(this.value);
         });
-    });
-
-    xhr.addEventListener('error', onError);
-
-    xhr.send();
-
-    function onError() {
-        table.innerHTML = 'Не удалось получить данные :( Попробуйте еще раз.';
+    } catch(err) {
+        return onError();
     }
 
-    function createTableHTML() {
-        let result = '<table><tr><th>Валюта</th><th>Покупка</th><th>Продажа</th></tr>';
+    function onError () {
+        table.innerHTML = 'Не вдалося отримати данні :( Спробуйте еще раз, або пізніше.';
+    }
+
+    function createTableHTML () {
+        let result = '<table><thead><tr><th>Валюта</th><th>Купівля</th><th>Продаж</th></tr></thead><tbody>';
 
         currencies.forEach(currency => {
             result += `<tr><td>${currency.ccy}</td><td>${currency.buy}</td><td>${currency.sale}</td></tr>`;
         });
 
-        result += '</table>';
+        result += '</tbody></table>';
 
         return result;
     }
 
-    function getCurrencyIndex(type) {
-        for (let i = 0; i < currencies.length; i++) {
-            if (currencies[i].ccy === type) {
-                return i;
-            }
-        }
-
-        return -1;
+    function getCurrencyIndex (type) {
+        return currencies.findIndex(currency => currency.ccy === type);
     }
 
-    function calcUAH(value) {
+    function calcUAH (value) {
         if (currencyIndex === -1) {
-            return {buy: 0, sale: 0};
+            return { buy: 0, sale: 0 };
         }
 
-        let buy = (currencies[currencyIndex].buy * value).toFixed(5);
-        let sale = (currencies[currencyIndex].sale * value).toFixed(5);
-        return {buy, sale};
+        let buy = (currencies[currencyIndex].buy * value).toFixed(2);
+        let sale = (currencies[currencyIndex].sale * value).toFixed(2);
+
+        return { buy, sale };
     }
 
-    function showUAH(value) {
+    function showUAH (value) {
         let UAH = calcUAH(value);
+
         uahResult.innerText = `${UAH.buy} / ${UAH.sale}`;
     }
 }());
